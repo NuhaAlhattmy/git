ad2a8c85d40a0664ea5be3cb50d66ea302eb35d4
Patrick Steinhardt <ps@pks.im>
2023-10-27 11:25:41 +0200

    ci: add support for GitLab CI
    We already support Azure Pipelines and GitHub Workflows in the Git
    project, but until now we do not have support for GitLab CI. While it is
    arguably not in the interest of the Git project to maintain a ton of
    different CI platforms, GitLab has recently ramped up its efforts and
    tries to contribute to the Git project more regularly.
    
    Part of a problem we hit at GitLab rather frequently is that our own,
    custom CI setup we have is so different to the setup that the Git
    project has. More esoteric jobs like "linux-TEST-vars" that also set a
    couple of environment variables do not exist in GitLab's custom CI
    setup, and maintaining them to keep up with what Git does feels like
    wasted time. The result is that we regularly send patch series upstream
    that fail to compile or pass tests in GitHub Workflows. We would thus
    like to integrate the GitLab CI configuration into the Git project to
    help us send better patch series upstream and thus reduce overhead for
    the maintainer.
    
    The integration does not necessarily have to be a first-class citizen,
    which would in practice only add to the fallout that pipeline failures
    have for the maintainer. That being said, we are happy to maintain this
    alternative CI setup for the Git project and will make test results
    available as part of our own mirror of the Git project at [1].
    
    This commit introduces the integration into our regular CI scripts so
    that most of the setup continues to be shared across all of the CI
    solutions.
    
    [1]: https://gitlab.com/gitlab-org/git
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/install-docker-dependencies.sh
+++ ci/install-docker-dependencies.sh
@@ -7,6 +7,9 @@
 
 begin_group "Install dependencies"
 
+# Required so that apt doesn't wait for user input on certain packages.
+export DEBIAN_FRONTEND=noninteractive
+
 case "$jobname" in
 linux32)
 	linux32 --32bit i386 sh -c '
@@ -16,8 +19,12 @@ linux32)
 	'
 	;;
 linux-musl)
-	apk add --update build-base curl-dev openssl-dev expat-dev gettext \
+	apk add --update git shadow sudo build-base curl-dev openssl-dev expat-dev gettext \
 		pcre2-dev python3 musl-libintl perl-utils ncurses >/dev/null
+	;;
+linux-*)
+	apt update -q &&
+	apt install -q -y sudo git make language-pack-is libsvn-perl apache2 libssl-dev libcurl4-openssl-dev libexpat-dev tcl tk gettext zlib1g-dev perl-modules liberror-perl libauthen-sasl-perl libemail-valid-perl libio-socket-ssl-perl libnet-smtp-ssl-perl ${CC_PACKAGE:-${CC:-gcc}}
 	;;
 pedantic)
 	dnf -yq update >/dev/null &&
--- ci/lib.sh
+++ ci/lib.sh
@@ -14,6 +14,22 @@ then
 		need_to_end_group=
 		echo '::endgroup::' >&2
 	}
+elif test true = "$GITLAB_CI"
+then
+	begin_group () {
+		need_to_end_group=t
+		echo -e "\e[0Ksection_start:$(date +%s):$(echo "$1" | tr ' ' _)\r\e[0K$1"
+		trap "end_group '$1'" EXIT
+		set -x
+	}
+
+	end_group () {
+		test -n "$need_to_end_group" || return 0
+		set +x
+		need_to_end_group=
+		echo -e "\e[0Ksection_end:$(date +%s):$(echo "$1" | tr ' ' _)\r\e[0K"
+		trap - EXIT
+	}
 else
 	begin_group () { :; }
 	end_group () { :; }
@@ -201,6 +217,39 @@ then
 	export GIT_PROVE_OPTS="--timer --jobs 10"
 	export GIT_TEST_OPTS="--verbose-log -x --github-workflow-markup"
 	MAKEFLAGS="$MAKEFLAGS --jobs=10"
+	test windows != "$CI_OS_NAME" ||
+	GIT_TEST_OPTS="--no-chain-lint --no-bin-wrappers $GIT_TEST_OPTS"
+elif test true = "$GITLAB_CI"
+then
+	CI_TYPE=gitlab-ci
+	CI_BRANCH="$CI_COMMIT_REF_NAME"
+	CI_COMMIT="$CI_COMMIT_SHA"
+	case "$CI_JOB_IMAGE" in
+	macos-*)
+		CI_OS_NAME=osx;;
+	alpine:*|ubuntu:*)
+		CI_OS_NAME=linux;;
+	*)
+		echo "Could not identify OS image" >&2
+		env >&2
+		exit 1
+		;;
+	esac
+	CI_REPO_SLUG="$CI_PROJECT_PATH"
+	CI_JOB_ID="$CI_JOB_ID"
+	CC="${CC_PACKAGE:-${CC:-gcc}}"
+	DONT_SKIP_TAGS=t
+	handle_failed_tests () {
+		create_failed_test_artifacts
+	}
+
+	cache_dir="$HOME/none"
+
+	runs_on_pool=$(echo "$CI_JOB_IMAGE" | tr : -)
+
+	export GIT_PROVE_OPTS="--timer --jobs $(nproc)"
+	export GIT_TEST_OPTS="--verbose-log -x"
+	MAKEFLAGS="$MAKEFLAGS --jobs=$(nproc)"
 	test windows != "$CI_OS_NAME" ||
 	GIT_TEST_OPTS="--no-chain-lint --no-bin-wrappers $GIT_TEST_OPTS"
 else
